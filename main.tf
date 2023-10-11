58decabba84d66f502d212323431f2ea010a0f33
jimb0 <jimccann@redhat.com>
2023-10-11 11:40:17 -0400

    1st

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "ibm_subnet" "portable_subnet" {
   ip_version = 4
   capacity   = var.subnet_capacity
   vlan_id    = ibm_network_vlan.ci_vlans[count.index].id
-  notes      = "portable_subnet"
+  notes      = "portable_subnet_ci"
   tags = var.vlan_tags
 
   //User can increase timeouts
